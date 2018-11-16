44536ff37e706bf5e8d965296db737422a7ebe5e
Maxwell <maxwell@makestud.io>
2018-11-16 14:48:21 +0000

    Added Assets from design zip

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCss = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', function() {
-  // place code for your default task here
-  console.log("YOLO");
-});
+sass.compiler = require('node-sass')
+
+gulp.task('sass', function(){
+   // We want to run "sass css/app.scss app.css --watch"
+   return gulp.src('css/app.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(
+        cleanCss({
+            compatability: 'ie8'
+        })
+    )
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.'))
+})
+
+
+gulp.task('watch', function(){
+    gulp.watch('css/app.scss', ['sass'])
+})
+
+gulp.task('default', ['sass', 'watch'])
